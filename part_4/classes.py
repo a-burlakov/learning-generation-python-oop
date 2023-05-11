b5f57465095ae1a37ffcd9bbb1eef63eb829cc7e
a-burlakov <fln@mail.ru>
2023-05-11 16:59:55 +0500

    learning: OOP

--- part_4/classes.py
+++ part_4/classes.py
@@ -1,3 +1,24 @@
 class Gun:
     def shoot(self):
         print("pif")
+
+
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.friends = 0
+
+    def add_friends(self, n):
+        self.friends += n
+
+
+class House:
+    def __init__(self, color, rooms):
+        self.color = color
+        self.rooms = rooms
+
+    def paint(self, new_color):
+        self.color = new_color
+
+    def add_rooms(self, n):
+        self.rooms += n
