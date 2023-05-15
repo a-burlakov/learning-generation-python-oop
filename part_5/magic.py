6c29d723cba6b37c0b668af1b5e40b33e1c2a307
a-burlakov <fln@mail.ru>
2023-05-15 16:42:40 +0500

    learning: OOP - magic methods.

---
+++ part_5/magic.py
@@ -0,0 +1,13 @@
+class Config:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __init__(self):
+        self.program_name = "GenerationPy"
+        self.environment = "release"
+        self.loglevel = "verbose"
+        self.version = "1.0.0"
