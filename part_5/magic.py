3eb212ed1ce3d91a2d1275407ac7017b94466b59
a-burlakov <fln@mail.ru>
2023-05-16 17:42:34 +0500

    learning: OOP - magic methods.

--- part_5/magic.py
+++ part_5/magic.py
@@ -210,3 +210,11 @@ class Calculator:
                 return a / b
             case _:
                 return None
+
+
+class RaiseTo:
+    def __init__(self, degree):
+        self.degree = degree
+
+    def __call__(self, x):
+        return x**self.degree
