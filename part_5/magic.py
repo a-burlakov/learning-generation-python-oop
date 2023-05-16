6c04cbbbc331146ff20837c69ecb18afc68f2da4
a-burlakov <fln@mail.ru>
2023-05-16 17:50:15 +0500

    learning: OOP - magic methods.

--- part_5/magic.py
+++ part_5/magic.py
@@ -287,4 +287,8 @@ class QuadraticPolynomial:
 
 
 class Strip:
-    ...
+    def __init__(self, chars):
+        self.chars = chars
+
+    def __call__(self, string):
+        return string.strip(self.chars)
