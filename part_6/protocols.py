935f26e8ee2216c8df7c468bd4250f988092477f
a-burlakov <fln@mail.ru>
2023-05-19 14:45:48 +0500

    learning: OOP - protocols

--- part_6/protocols.py
+++ part_6/protocols.py
@@ -20,3 +20,29 @@ class Point:
             self.y,
             self.z,
         )
+
+
+class DevelopmentTeam:
+    def __init__(self):
+        self._juniors = []
+        self._seniors = []
+
+    def add_junior(self, *args):
+        self._juniors.extend(args)
+
+    def add_senior(self, *args):
+        self._seniors.extend(args)
+
+    def __iter__(self):
+        for dev in self._juniors:
+            yield dev, "junior"
+        for dev in self._seniors:
+            yield dev, "senior"
+
+
+beegeek = DevelopmentTeam()
+
+beegeek.add_junior("Timur")
+beegeek.add_junior("Arthur", "Valery")
+beegeek.add_senior("Gvido")
+print(*beegeek, sep="\n")
