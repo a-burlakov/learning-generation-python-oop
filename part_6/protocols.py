7d1b36844154effad0017a14f79b97ea008f9cc2
a-burlakov <fln@mail.ru>
2023-05-19 14:38:55 +0500

    learning: OOP - protocols

--- part_6/protocols.py
+++ part_6/protocols.py
@@ -1,6 +1,22 @@
-non_zero = filter(None, [-2, -1, 0, 1, 2])
-positive = map(abs, non_zero)
-print(non_zero)
-print(positive)
-print(list(non_zero))
-print(list(positive))
+class Order:
+    def __init__(self, cart, customer):
+        self.cart = list(cart)  # список покупок
+        self.customer = customer  # имя покупателя
+
+    def __iter__(self):
+        return iter(self.cart)
+
+
+class Point:
+    def __init__(self, x, y, z):
+        self.x, self.y, self.z = x, y, z
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.x}, {self.y}, {self.z})"
+
+    def __iter__(self):
+        yield from (
+            self.x,
+            self.y,
+            self.z,
+        )
