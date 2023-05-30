43242cb5266e8306e38548c2e7f51969291762ba
a-burlakov <fln@mail.ru>
2023-05-30 15:27:21 +0500

    learning: OOP - inheritance

---
+++ part_7/inheritance_and_polymorphism.py
@@ -0,0 +1,34 @@
+class Vehicle:
+    ...
+
+
+class LandVehicle(Vehicle):
+    ...
+
+
+class WaterVehicle(Vehicle):
+    ...
+
+
+class AirVehicle(Vehicle):
+    ...
+
+
+class Car(LandVehicle):
+    ...
+
+
+class Motocycle(LandVehicle):
+    ...
+
+
+class Bicycle(LandVehicle):
+    ...
+
+
+class Propeller(AirVehicle):
+    ...
+
+
+class Jet(AirVehicle):
+    ...
