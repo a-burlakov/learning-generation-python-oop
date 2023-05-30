8c0471683ad663dda125f60a227958d013afae73
a-burlakov <fln@mail.ru>
2023-05-30 15:33:20 +0500

    learning: OOP - inheritance

--- part_7/inheritance_and_polymorphism.py
+++ part_7/inheritance_and_polymorphism.py
@@ -32,3 +32,88 @@ class Propeller(AirVehicle):
 
 class Jet(AirVehicle):
     ...
+
+
+class Shape:
+    ...
+
+
+class Circle(Shape):
+    ...
+
+
+class Polygon(Shape):
+    ...
+
+
+class Quadrilateral(Polygon):
+    ...
+
+
+class Triangle(Polygon):
+    ...
+
+
+class Parallelogram(Quadrilateral):
+    ...
+
+
+class Rectangle(Parallelogram):
+    ...
+
+
+class Square(Rectangle):
+    ...
+
+
+class IsoscelesTriangle(Triangle):
+    ...
+
+
+class EquilateralTriangle(Triangle):
+    ...
+
+
+# # # # # # # # # # # # # # # # # #
+
+
+class Animal:
+    def sleep(self):
+        ...
+
+    def eat(self):
+        ...
+
+
+class Fish(Animal):
+    def swim(self):
+        ...
+
+
+class Bird(Animal):
+    def lay_eggs(self):
+        ...
+
+
+class FlyingBird(Bird):
+    def fly(self):
+        ...
+
+
+# # # # # # # # # # # # # # # #
+
+
+class User:
+    def __init__(self, name):
+        self.name = name
+
+    def skip_ads(self):
+        return False
+
+
+class PremiumUser(User):
+    def skip_ads(self):
+        return True
+
+
+# # # # # # # # # # # # # # # #
